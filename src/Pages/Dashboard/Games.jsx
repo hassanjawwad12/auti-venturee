66b695c27817947feb7d27c3b4c17ba34566a1cf
Muhammad Hassan Jawwad <hassanjawwad01@gmail.com>
2024-06-10 12:18:59 +0500

    sense of belonging

--- src/Pages/Dashboard/Games.jsx
+++ src/Pages/Dashboard/Games.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { Image } from "@chakra-ui/react";
 
 const Games = () => {
   return (
   <>
    <Navbar />
+   <Image src="lion2.png" width={'100%'} height={'40vh'} alt="Games" />
    
   </>
   )
