b9948b69dd47674ef8d40c9cfe4e0285723ba416
Muhammad Hassan Jawwad <hassanjawwad01@gmail.com>
2024-06-10 11:47:05 +0500

    login complete

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Footer from "./Footer";
-import { Box, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  VStack,
+  Text,
+  Button,
+  Icon,
+  Input,
+  Checkbox,
+  HStack,
+} from "@chakra-ui/react";
+import { FaRegUser } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <>
-    <Box
-        backgroundImage="url('/landing.jpg')"
+      <Box
+        backgroundImage="url('/login.jpg')"
         backgroundSize="cover"
         backgroundPosition="center"
         height="100vh"
@@ -14,18 +29,95 @@ const Login = () => {
       >
         <VStack
           position="absolute"
-          top={["52%", "52%"]}
-          left="35%"
-          transform="translate(-50%, -50%)"
+          mt={["15%", "5%"]}
+          mr={[0, "10%"]}
+          ml={["4%", "25%"]}
           align={"center"}
-          mt={1}
+          bg={"#41807c"}
+          p={4}
+          borderRadius={"16px"}
+          width={["90%", "50%"]}
+          height={"80vh"}
+          justifyContent={"center"}
+          spacing={8}
         >
+          <Icon as={FaRegUser} w={20} h={20} color={"#f1c50e"} />
+          <Text
+            fontSize={["lg", "5xl"]}
+            fontWeight={"semibold"}
+            color={"#f1c50e"}
+          >
+            User Login
+          </Text>
 
+          <Input
+            rounded={"16px"}
+            height={"7vh"}
+            width={"80%"}
+            p={4}
+            bg={"white"}
+            color={"black"}
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            rounded={"16px"}
+            height={"7vh"}
+            width={"80%"}
+            p={4}
+            bg={"white"}
+            color={"black"}
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <HStack width={"80%"} justify={'flex-end'} align={'flex-end'}>
+            <Text color={"white"}>Forgot Password?</Text>
+          </HStack>
+
+          <HStack width={"80%"} align={"flex-start"}>
+            <Checkbox color={"white"} defaultChecked>
+              Remember Me
+            </Checkbox>
+          </HStack>
+          <Button
+            width={"80%"}
+            px={8}
+            py={6}
+            rounded={"20px"}
+            color={"black"}
+            bg={"#f1c50e"}
+            size={"lg"}
+            _hover={{
+              bg: "#2cccc4",
+              color: "black",
+            }}
+            onClick={() => navigate("/login")}
+          >
+            Login
+          </Button>
+          <Text
+            fontSize={"20px"}
+            color={"white"}
+            as={"i"}
+            fontWeight={'semibold'}
+            onClick={() => navigate("/register")}
+            cursor={'pointer'}
+          >
+            Don't have an account? Click here
+          </Text>
         </VStack>
-        </Box>      
+      </Box>
+
       <Footer />
     </>
   );
 };
 
 export default Login;
+
+/*
+
+*/
